8c556908a7ff9f6d240100ee41810ce5c83e71a5
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-03-13 15:43:42 -0300

    ERROR 404 (Not Found)

---
+++ config/config/prod.js
@@ -0,0 +1,11 @@
+// Production Keys
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+}
+
+// Prod URI
+// prodURI: 'mongodb+srv://myuser:TOQBMxnsVWZpZAqZ@cluster0-rekjb.mongodb.net/test?retryWrites=true&w=majority'
