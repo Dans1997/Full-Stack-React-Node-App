cb8f1974a4a138e02ac9c0b9a0070678344d1eac
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-03-16 09:15:52 -0300

    Delete User.js

--- config/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const userSchema = new Schema({
-    googleId: String
-});
-
-// Create a New Collection of Users
-mongoose.model('users', userSchema);
